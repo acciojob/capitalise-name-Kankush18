456fc7afbf79c184d7626c8f73a8875eaed9e5d1
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 12:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+  const nameInput = document.getElementById('fname');
+
+ 
+  nameInput.addEventListener('click', function(event) {
+    
+    nameInput.value = nameInput.value.toUpperCase();
+  });
